070d84989c8f930dea4b6f7401a04fc3a3b8b2eb
venkatareddy369 <102140502+venkatareddy369@users.noreply.github.com>
2022-04-18 22:59:14 +0530

    intial commit

---
+++ netflix/src/C data/index.js
@@ -0,0 +1,86 @@
+const cdata = [
+    {
+        imgSrc: 'https://img.fresherslive.com/latestnews/images/opt-images/width-900/2022/02/05/fbd-61fe6e17aa8fb-1644064279.jpg',
+        
+        title:'A youtube web series ',
+        sname:'Samajavaragamana',
+        link:'https://youtu.be/d3Dv0_kR7oU'
+      
+    },
+    {
+        imgSrc:"https://ragala.xyz/wp-content/uploads/2021/09/The-Software-DevLOVEper-Movie-Shanmukh-Jaswanth-Vaishnavi.jpg",
+        
+        title:' A youtube web series ',
+        sname:'Software developer',
+        link:'https://youtu.be/ZvISbJNSolg',  
+    },
+    {
+        imgSrc:'https://i.ytimg.com/vi/p3XTZlZ6-xw/maxresdefault.jpg',
+        
+        title:' A youtube web series',
+        sname:'Surya',
+        link:'https://youtu.be/p3XTZlZ6-xw'
+    },
+    {
+        imgSrc:'https://i.ytimg.com/vi/fZ2YfkBJ8PY/maxresdefault.jpg',
+        title:' A youtube web series',
+        sname:'Bebamma' ,
+        link:'https://youtu.be/9NQQmd9cVgo'
+      
+    },
+    {
+        imgSrc:'https://i.ytimg.com/vi/2QcWpVhSn8o/maxresdefault.jpg',
+        title:' A youtube we series',
+        sname:'Charminar',
+        link:'https://youtu.be/2QcWpVhSn8o'
+      
+    },
+    {
+    imgSrc: 'https://i.ytimg.com/vi/x2LjLtXT1Pc/hqdefault.jpg',
+    title:' A youtube series',
+    sname:'Galat Shot',
+    link:'https://youtu.be/x2LjLtXT1Pc'
+  
+    },
+    {
+        imgSrc: 'https://i.ytimg.com/vi/BwYR6vsMBfc/maxresdefault.jpg',
+        title:' A youtube series',
+        sname:'Missamma',
+        link:'https://youtu.be/BwYR6vsMBfc'
+    },
+    {
+        imgSrc: 'https://i.ytimg.com/vi/XmzWBuygAFU/maxresdefault.jpg',
+        title:' A youtube series',
+        sname:'Misunderstanding',
+        link:'https://youtu.be/XmzWBuygAFU'
+    },
+    {
+        imgSrc: 'https://english.sakshi.com/sites/default/files/article_images/2021/08/6/423BD19F-B1BF-4960-A9B6-59B59267C639-1628246197.jpeg',
+        title:' A youtube series',
+        sname:'Back bencher',
+        link:'https://youtu.be/jY-Ud5s85pM'
+    },
+    {
+        imgSrc: 'https://i.ytimg.com/vi/GCxyQ3uRWrQ/maxresdefault.jpg',
+        title:' A youtube series',
+        sname:'Prem kahani',
+        link:'https://youtu.be/GCxyQ3uRWrQ'
+    },
+    {
+        imgSrc: 'https://i.ytimg.com/vi/58cehZ5eUck/maxresdefault.jpg',
+        title:' A youtube series',
+        sname:'The Assistant director',
+        link:'https://youtu.be/58cehZ5eUck'
+    },
+    {
+        imgSrc: 'https://i.ytimg.com/vi/d9nxGrYGeeU/maxresdefault.jpg',
+        title:' A youtube series',
+        sname:'Fidaa',
+        link:'https://youtu.be/d9nxGrYGeeU'
+    }
+
+
+    
+]
+
+export default cdata;
---
+++ netflix/src/container/App/App.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import Card from '../../components/Card';
+
+import cdata from '../../../src/cdata';
+
+// console.log(cdata[1].title);
+const App = () => {
+  return (
+    <>
+      <h1 className='heading_style'>List of top Netflix series in 2021</h1>
+      <Card
+        imgSrc={cdata[0].imgSrc}
+        title={cdata[0].title}
+        link={cdata[0].link}
+        sname={cdata[0].sname}
+      />
+      <Card
+        imgSrc={cdata[1].imgSrc}
+        title={cdata[1].title}
+        link={cdata[1].link}
+        sname={cdata[1].sname}
+      />
+      <Card
+        imgSrc={cdata[2].imgSrc}
+        title={cdata[2].title}
+        link={cdata[2].link}
+        sname={cdata[2].sname}
+      />
+      <Card
+        imgSrc={cdata[3].imgSrc}
+        title={cdata[3].title}
+        link={cdata[3].link}
+        sname={cdata[3].sname}
+      />
+      <Card
+        imgSrc={cdata[4].imgSrc}
+        title={cdata[4].title}
+        link={cdata[4].link}
+        sname={cdata[4].sname}
+      />
+      <Card
+        imgSrc={cdata[5].imgSrc}
+        title={cdata[5].title}
+        link={cdata[5].link}
+        sname={cdata[5].sname}
+      />
+        <Card
+        imgSrc={cdata[6].imgSrc}
+        title={cdata[6].title}
+        link={cdata[6].link}
+        sname={cdata[6].sname}
+      />
+      <Card
+        imgSrc={cdata[7].imgSrc}
+        title={cdata[7].title}
+        link={cdata[7].link}
+        sname={cdata[7].sname}
+      />
+        <Card
+        imgSrc={cdata[8].imgSrc}
+        title={cdata[8].title}
+        link={cdata[8].link}
+        sname={cdata[8].sname}
+      />
+        <Card
+        imgSrc={cdata[9].imgSrc}
+        title={cdata[9].title}
+        link={cdata[9].link}
+        sname={cdata[9].sname}
+      />
+       <Card
+        imgSrc={cdata[10].imgSrc}
+        title={cdata[10].title}
+        link={cdata[10].link}
+        sname={cdata[10].sname}
+      />
+        <Card
+        imgSrc={cdata[11].imgSrc}
+        title={cdata[11].title}
+        link={cdata[11].link}
+        sname={cdata[11].sname}
+      />
+        
+    </>
+
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
